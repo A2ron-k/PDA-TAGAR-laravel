dd03846a9d851e2cd09022cd6c6a72c887f3bcfa
a2ron-k <aaronkwah@gmail.com>
2022-07-02 21:38:33 +0800

    minor css patch

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <title>@yield('title')</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{url('css/site.css')}}">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -21,52 +22,6 @@
             });
         })
     </script>
-    <style>
-        body {
-            overflow-x: hidden;
-        }
-
-        #sidebar-wrapper {
-            min-height: 100vh;
-            margin-left: -15rem;
-            -webkit-transition: margin .25s ease-out;
-            -moz-transition: margin .25s ease-out;
-            -o-transition: margin .25s ease-out;
-            transition: margin .25s ease-out;
-        }
-
-        #sidebar-wrapper .sidebar-heading {
-            padding: 0.875rem 1.25rem;
-            font-size: 1.2rem;
-        }
-
-        #sidebar-wrapper .list-group {
-            width: 15rem;
-        }
-
-        #page-content-wrapper {
-            min-width: 100vw;
-        }
-
-        #wrapper.toggled #sidebar-wrapper {
-            margin-left: 0;
-        }
-
-        @media (min-width: 768px) {
-            #sidebar-wrapper {
-                margin-left: 0;
-            }
-
-            #page-content-wrapper {
-                min-width: 0;
-                width: 100%;
-            }
-
-            #wrapper.toggled #sidebar-wrapper {
-                margin-left: -15rem;
-            }
-        }
-    </style>
 </head>
 
 <body>
@@ -75,9 +30,10 @@
         <div class="bg-light border-right" id="sidebar-wrapper">
 
             <div class="list-group list-group-flush">
-                
+
                 <a href="#" class="list-group-item list-group-item-action bg-light">
-                    <img src="https://img.freepik.com/free-vector/cow-farm-logo_42174-13.jpg?w=2000" width="50px"alt=""> Current Case</a>
+                    <img src="https://img.freepik.com/free-vector/cow-farm-logo_42174-13.jpg?w=2000"
+                        width="50px"alt=""> Current Case</a>
                 <a href="#" class="list-group-item list-group-item-action bg-light">Case History</a>
                 <a href="#" class="list-group-item list-group-item-action bg-light">Simulation</a>
                 {{-- <a href="{{ route('case.index') }}" class="list-group-item list-group-item-action bg-light">OMNI</a> --}}
@@ -96,6 +52,8 @@
                 <main class="py-4">
                     @yield('content')
                 </main>
+
+                @yield('script')
             </div>
         </div>
 
