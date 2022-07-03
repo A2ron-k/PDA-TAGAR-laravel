00ce01d6ed1ee95f613a6dfe72609d739040455a
godwinnwong <108579289+godwinnwong@users.noreply.github.com>
2022-07-03 17:34:24 +0800

    added 2 indepth floorplan

--- resources/views/geolocation.blade.php
+++ resources/views/geolocation.blade.php
@@ -10,16 +10,17 @@
 
 <div id= "geolocation-container-case" style="top: 108px;">
     {{-- <img src="" alt=""> --}}
-    <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=768861+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/farm-gps/">tractor gps</a></iframe></div>
+    <div style="width: 100%"><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=399738+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/farm-gps/">tractor gps</a></iframe></div>
     {{-- <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q={{ $case['postal_code']}}+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/farm-gps/">tractor gps</a></iframe></div> --}}
 </div>
 
-<div id= "geolocation-container-case" style="top: 609px;">
-    <img src="" alt="">
+<div id= "geolocation-container-case" style="top: 609px; height:50%">
+    <div style="width: 100%"><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.moricondo.sg/wp-content/uploads/2021/07/Project_Fascade_For_MORI_Condo_at_Guillemard_Road_Jalan_Molek_Geylang_By_Roxy_Pacific.jpg" alt="">tractor gps</a></iframe></div>
+    {{-- <img src="https://www.moricondo.sg/wp-content/uploads/2021/07/Project_Fascade_For_MORI_Condo_at_Guillemard_Road_Jalan_Molek_Geylang_By_Roxy_Pacific.jpg" alt=""> --}}
 </div>
 
 <div id= "geolocation-container-case" style="top: 1125px;">
-    <img src="" alt="">
+    <img src="https://mori-residences.sg/wp-content/uploads/2021/11/Mori-Residences-Sitemap-Large.jpg" alt="">
 </div>
 
 
